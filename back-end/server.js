3a1ee313bebb70d283fd9b74486a02b6002d38a7
Ali Hassan <alihere4587@gmail.com>
2026-03-28 22:16:37 +0500

    #

--- back-end/server.js
+++ back-end/server.js
@@ -7,7 +7,7 @@ import { requestLogger } from "./middlewares/requestLogger.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
