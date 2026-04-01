9035165ba054f60a473c6584e9e6c83d13839f32
Ali Hassan <alihere4587@gmail.com>
2026-04-01 20:56:08 +0500

    Made the backend to send json data to front instead of the html

--- back-end/controllers/resumeAnalyzerController.js
+++ back-end/controllers/resumeAnalyzerController.js
@@ -1,6 +1,5 @@
 import parseResume from "../utils/parseResume.cjs";
 import analyzeResume from "../services/aiService.js";
-import generateResumeHTML from "../services/resumeGenerator.js";
 
 const resumeAnalyzerController = async (req, res) => {
   try {
@@ -15,8 +14,7 @@ const resumeAnalyzerController = async (req, res) => {
 
     // 2. AI Analysis
     const analysis = await analyzeResume(resumeText);
-    const generatedResumeHtml = generateResumeHTML();
-    res.json({ analysis, resume: generatedResumeHtml });
+    res.json(analysis);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
