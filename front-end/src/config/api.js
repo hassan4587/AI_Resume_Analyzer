969b3445bc13315f8c14d44741491d4698f83672
Ali Hassan <alihere4587@gmail.com>
2026-04-05 18:54:34 +0500

    #

--- front-end/src/config/api.js
+++ front-end/src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
 
-const API_URL = "http://localhost:5000";
-const API_URL_PROD = import.meta.env.VITE_API_BASE_URL;
+// const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 export { API_URL };
